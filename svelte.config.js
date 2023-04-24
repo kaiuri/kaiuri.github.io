0a4434c9a2c90db3ca18aef72b7141314c656aa1
kaiuri <uriel.acioli@gmail.com>
2023-04-24 10:30:25 -0300

    initial commit

---
+++ svelte.config.js
@@ -0,0 +1,19 @@
+import adapter from '@sveltejs/adapter-static';
+const dev = process.argv.includes('dev');
+
+export default {
+  kit: {
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: "index.html",
+      precompress: false,
+      strict: true,
+      paths: {
+        base: dev ? '' : process.env.BASE_PATH,
+      }
+    })
+  }
+};
