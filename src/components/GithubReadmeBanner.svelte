38cef140f7104dfd7e39cac321855e9cb9e60a0f
kaiuri <uriel.acioli@gmail.com>
2023-04-24 13:40:30 -0300

    Backup

---
+++ src/components/GithubReadmeBanner.svelte
@@ -0,0 +1,44 @@
+<svelte:options immutable={true} />
+
+<script lang="ts" context="module">
+  interface Params extends Record<string, string> {
+    maxLangs: string;
+    user: string;
+    layout: "default" | "compact";
+    hide: string;
+    theme: string;
+  }
+</script>
+
+<script lang="ts">
+  let params: Params = {
+    maxLangs: "15",
+    user: "kaiuri",
+    layout: "compact",
+    hide: [
+      // "html",
+      // "makefile",
+      // "fennel",
+      "scheme",
+      "javascript",
+      // "clojure",
+      "lua",
+      // "shell",
+      "css",
+    ].join(","),
+    theme: "discord_old_blurple",
+  };
+</script>
+
+<img
+  src={"https://github-readme-stats.vercel.app/api/top-langs/?" +
+    new URLSearchParams(params)}
+  alt="Github Banner"
+/>
+
+<style>
+  img {
+    width: 100%;
+    height: 50%;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,8 @@
 <script lang="ts">
+  import GithubReadmeBanner from "../components/GithubReadmeBanner.svelte";
 </script>
 
-<h1
-  class={[
-    "text-3xl",
-    "font-bold",
-    "p-2",
-    // align center
-    "content-end",
-  ].join(" ")}
->
-  Hello world!
+<h1 class={["text-3xl", "font-bold", "p-2", "content-end"].join(" ")}>
+  Whaaaaat
 </h1>
+<GithubReadmeBanner />
